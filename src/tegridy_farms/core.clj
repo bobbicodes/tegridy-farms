24162fd2611760522e7375de0c584042c5e45a5b
porkostomus <porkostomus@mail.com>
2019-11-29 16:11:13 -0800

    Twitter integration

--- src/tegridy_farms/core.clj
+++ src/tegridy_farms/core.clj
@@ -1,24 +1,39 @@
 (ns tegridy-farms.core
-  (:require [clj-http.client :as client]
-            [clojure.data.json :as json]))
+  (:require [clojure.edn :as edn]
+            [clj-http.client :as client]
+            [clojure.data.json :as json]
+            [twitter.oauth :as oauth]
+            [twitter.api.restful :as rest]))
 
 ;; poll a GitHub repo, look for new pull requests
 ;; GET /repos/:owner/:repo/pulls
 
+(def github-oauth-token (:oauth-token (:github (edn/read-string (slurp "config.edn")))))
+
+(def twitter-creds
+  (let [{:keys [api-key api-secret-key access-token access-token-secret]} 
+        (:twitter (edn/read-string (slurp "config.edn")))]
+    (oauth/make-oauth-creds
+     api-key api-secret-key access-token access-token-secret)))
+
 (defn pr-list []
   (client/get "https://api.github.com/repos/porkostomus/tegridy-farms/pulls"
-              {:oauth-token (slurp "config.edn")}))
+              {:oauth-token github-oauth-token}))
 
 ;; get a single pull request
 ;; GET /repos/:owner/:repo/pulls/:pull_number
 
 (defn get-pr [n]
   (client/get (str "https://api.github.com/repos/porkostomus/tegridy-farms/pulls/" n)
-              {:oauth-token (slurp "config.edn")}))
+              {:oauth-token github-oauth-token}))
+
+(defn tweet [msg]
+  (rest/statuses-update :oauth-creds twitter-creds :params {:status msg}))
 
 (comment
  (keys (first (json/read-str 
                (:body (pr-list))
                :key-fn keyword)))
- (json/read-str (:body (get-pr 1)) :key-fn keyword)
+ (keys (json/read-str (:body (get-pr 1)) :key-fn keyword))
+  (tweet "Testing simpler creds function.")
  )
