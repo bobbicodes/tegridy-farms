bd0abab214f91bf17b5aa8d98b506873cd321950
porkostomus <porkostomus@mail.com>
2019-11-29 23:11:02 -0800

    create database table

--- src/tegridy_farms/core.clj
+++ src/tegridy_farms/core.clj
@@ -2,6 +2,7 @@
   (:require [clojure.edn :as edn]
             [clj-http.client :as client]
             [clojure.data.json :as json]
+            [next.jdbc :as jdbc]
             [twitter.oauth :as oauth]
             [twitter.api.restful :as rest]))
 
@@ -28,17 +29,35 @@
 (defn tweet [msg]
   (rest/statuses-update :oauth-creds twitter-creds :params {:status msg}))
 
+(defn char-limit [n s]
+  (if (< n (count s))
+    (str (apply str (take n s)) "...")
+    s))
+
 (defn pr-summary [pr]
   (let [{:keys [number title body html_url]} pr]
-    (str "PR#" number " 
-         " "Title: " title " 
-         " "Body: " body " 
-         " "URL: " html_url)))
+    (str "PR #" number ": " (char-limit 60 title) "\n"
+         (char-limit 180 body)
+         html_url)))
+
+(def db {:dbtype "postgresql"
+         :dbname "tegridy"
+         :user "postgres"
+         :password "pwd"})
+
+(def ds (jdbc/get-datasource db))
+
+#_(jdbc/execute! ds ["
+create table requests (
+  id serial primary key,
+  title varchar (255) not null,
+  body varchar (255) not null,
+  url varchar(255) not null
+)"])
 
 (comment
- (tweet (pr-summary (first (json/read-str 
+ (tweet (pr-summary (first (json/read-str
                             (:body (pr-list))
                             :key-fn keyword))))
- (keys (json/read-str (:body (get-pr 1)) :key-fn keyword))
-  (tweet "Testing simpler creds function.")
+ (pr-summary (json/read-str (:body (get-pr 1)) :key-fn keyword))
  )
