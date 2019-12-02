94ad75421494679764f2656debf1daf6803d211a
porkostomus <porkostomus@mail.com>
2019-12-01 22:43:07 -0800

    running program

--- README.md
+++ README.md
@@ -2,30 +2,24 @@
 
 HTTP Integration with GitHub and Twitter
 
-Write a program that will poll a GitHub repo, look for new pull requests, and tweet a [summary of each PR](https://developer.github.com/v3/pulls/) to Twitter.
+Upon execution, pull requests to this repo will be posted to the [TegridyFarms2](https://twitter.com/TegridyFarms2) twitter account.
 
-Some practical considerations:
+## Configuration
 
-* A dummy github repo should be created and API access be enabled
+You must supply your Github and Twitter credentials in a file named `config.edn`:
 
-* A [dummy twitter account](https://twitter.com/TegridyFarms2) should be created and API access should be enabled
+```edn
+{:github {:oauth-token "oauth token"}
+ :twitter {:api-key "api key"
+           :api-secret-key "api secret key"
+           :access-token "access token"
+           :access-token-secret "access token secret"}}
+```
 
-* Getting the PRs via webhook might be impractical (especially considering that we should be able to easily run and test your program).
+## Usage
 
-Suggestion: the program can start up, gather the latest PR's, post them to Twitter, then exit. Subsequent runs of the program should pick up PR's that have come in since the last time. (How will you persist the state between runs?) .... Alternately, the program can run forever and periodically poll the GitHub repo for changes.
-
-Constraints
-
-Make plain HTTP requests against GitHub and Twitter, i.e., do not use a 3rd party Github or Twitter SDK.
-
-POST /repos/:owner/:repo/pulls
-
-## Development
-
-* Make sure [postgres](https://www.postgresql.org/) is installed and available on your `$PATH`. Launch postgres and create local db:
+* Make sure you have the Clojure CLI tools installed, and run:
 
 ```bash
-initdb pg
-pg_ctl -D pg -l logfile start
-createdb tegridy
+clj -m tegridy-farms.core
 ```
--- src/tegridy_farms/core.clj
+++ src/tegridy_farms/core.clj
@@ -2,7 +2,6 @@
   (:require [clojure.edn :as edn]
             [clj-http.client :as client]
             [clojure.data.json :as json]
-            [next.jdbc :as jdbc]
             [twitter.oauth :as oauth]
             [twitter.api.restful :as rest]))
 
@@ -16,21 +15,29 @@
     (oauth/make-oauth-creds api-key api-secret-key access-token access-token-secret)))
 
 (defn pr-list []
-  (client/get "https://api.github.com/repos/porkostomus/tegridy-farms/pulls"
-              {:oauth-token github-oauth-token}))
+  (try
+    (client/get "https://api.github.com/repos/porkostomus/tegridy-farms/pulls"
+                {:oauth-token github-oauth-token})
+    (catch Exception e (println (str "Caught exception: " e)))))
 
 ;; get a single pull request
 ;; GET /repos/:owner/:repo/pulls/:pull_number
 
 (defn get-pr [n]
-  (client/get (str "https://api.github.com/repos/porkostomus/tegridy-farms/pulls/" n)
-              {:oauth-token github-oauth-token}))
+  (try
+    (client/get (str "https://api.github.com/repos/porkostomus/tegridy-farms/pulls/" n)
+                {:oauth-token github-oauth-token})
+    (catch Exception e (println (str "Caught exception: " e)))))
 
 (defn tweet [msg]
-  (rest/statuses-update :oauth-creds twitter-creds :params {:status msg}))
+  (try 
+    (rest/statuses-update :oauth-creds twitter-creds :params {:status msg})
+    (catch Exception e (println (str "Caught exception: " e)))))
 
 (defn latest-tweet []
-  (rest/statuses-user-timeline :oauth-creds twitter-creds :params {:count 1}))
+  (try
+    (rest/statuses-user-timeline :oauth-creds twitter-creds :params {:count 1})
+    (catch Exception e (println (str "Caught exception: " e)))))
 
 (defn char-limit [n s]
   (if (< n (count s))
@@ -43,42 +50,40 @@
          (char-limit 180 body)
          html_url)))
 
-(def db {:dbtype "postgresql"
-         :dbname "tegridy"
-         :user "postgres"
-         :password "pwd"})
-
-(def ds (jdbc/get-datasource db))
-
-(defn create-requests-table! []
-  (jdbc/execute! ds ["
-create table requests (
-  id serial primary key,
-  title varchar (255) not null,
-  body varchar (255) not null,
-  url varchar (255) not null
-)"]))
-
-;; need to escape single-quotes
-
-(defn insert-request [request]
-  (let [{:keys [title body html_url]} request]
-    (jdbc/execute! ds [(str "
-insert into requests(title, body, url)
-  values('" (char-limit 250 title) "', '" (char-limit 250 body) "', '" html_url "')")])))
-
 (defn last-pr-tweeted
   "Returns the number of the most recently posted PR"
   []
-  (Integer/parseInt (str (nth (:text (first (:body (latest-tweet)))) 4))))
+  (let [pr-num (re-find #"PR #\d+"  (:text (first (:body (latest-tweet)))))]
+    (Integer/parseInt (subs pr-num 4))))
+
+(defn new-pr-nums
+  "Returns list of numbers of PRs on Github"
+  []
+  (map :number
+       (json/read-str
+        (:body (pr-list))
+        :key-fn keyword)))
+
+(defn untweeted-prs []
+  (filter #(< (last-pr-tweeted) %) (new-pr-nums)))
+
+(defn -main []
+  (if-let [tweets (seq (untweeted-prs))]
+    (let [next-tweet (first (sort tweets))]
+      (println (str "Tweeting PR #" next-tweet " ..."))
+      (tweet (pr-summary (json/read-str (:body (get-pr next-tweet)) :key-fn keyword)))
+      (println "Will check for more in 1 minute..."))
+    (println "No PRs to tweet. Will try again in 1 minute..."))
+  (Thread/sleep 60000)
+  (-main))
 
 (comment
-  (= 7 (last-pr-tweeted))
-  
+  (-main)
+  (last-pr-tweeted)
+  (sort (untweeted-prs))
+ (tweet (pr-summary (json/read-str (:body (get-pr (first (sort (untweeted-prs))))) :key-fn keyword)))
  (Integer/parseInt (str (nth (:text (first (:body (latest-tweet)))) 4)))
  (tweet (pr-summary (first (json/read-str
-                                     (:body (pr-list))
-                                     :key-fn keyword))))
-  (insert-request (json/read-str (:body (get-pr 8)) :key-fn keyword))
-  (jdbc/execute! ds ["select * from requests"])
+                                   (:body (pr-list))
+                                   :key-fn keyword))))
   )
